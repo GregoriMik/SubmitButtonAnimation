f26964f03a6b6d802ff8da8e0d89d20a20ee553c
Grzegorz Mikina <gregorimikina@gmail.com>
2023-03-02 00:00:16 +0100

    Create simple submit button animation

---
+++ main.js
@@ -0,0 +1,27 @@
+// const btn1 = document.getElementById("btn1");
+
+// function newText() {
+//     console.log("klik");
+//     btn1.addEventListener("click", () => {
+//         console.log('klikam btn1');
+//         btn1.classList.add("clicked");
+//         setTimeout(() => {
+//             btn1.classList.remove("clicked");
+//         }, 4000);
+//     });
+// }
+
+// const btn1 = document.getElementById('btn1');
+// const btn2 = document.getElementById('btn2');
+// const btn3 = document.getElementById('btn3');
+// const btn4 = document.getElementById('btn4');
+ 
+ 
+// function newText() {
+//     preventDefault();
+//     target.value = "Dodano";
+//     setTimeout(function() {
+//         target.value = "Dodaj do koszyka";
+//         submit();
+//     }, 5000);
+// }
